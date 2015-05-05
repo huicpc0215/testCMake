25411898bcbcd541e47d39f873901c5bb4a04034
huicpc0215 <huicpc0215@gmail.com>
2015-05-05 15:47:01 +0800

    update gitignore

---
+++ Demo_3/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include"math/MyAdditions.h"
+using namespace std;
+
+int main(int argc,char *argv[]){
+    if( argc < 3 ){
+        fprintf(stdout,"arguments too few \n");
+        return 0;
+    }
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    fprintf(stdout,"using own addition method : %d + %d = %d\n",a,b,add(a,b));
+    return 0;
+}
