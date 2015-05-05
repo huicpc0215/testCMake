9f0bd3220c5232b697eb135902c90db216bdae27
huicpc0215 <huicpc0215@gmail.com>
2015-05-05 15:13:49 +0800

    update Demo_2

---
+++ Demo_2/MyAdditions.cpp
@@ -0,0 +1,6 @@
+/*
+ * calulate the sum of two number
+ */
+int add(int a,int b){
+    return a+b;
+}
---
+++ Demo_2/MyAdditions.h
@@ -0,0 +1,6 @@
+#ifndef ADDITION_H
+#define ADDITION_H
+
+extern int add(int a,int b);
+
+#endif
