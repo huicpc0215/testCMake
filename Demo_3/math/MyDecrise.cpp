e89759f477eb3050928796da0ef05e7e3025b5d7
huicpc0215 <huicpc0215@gmail.com>
2015-05-06 14:40:47 +0800

    update Demo_3

---
+++ Demo_3/math/MyDecrise.cpp
@@ -0,0 +1,7 @@
+/*
+ * calulate the sum of two number
+ */
+#include"MyDecrise.h"
+int dec(int a,int b){
+    return a-b;
+}
---
+++ Demo_3/math/MyDecrise.h
@@ -0,0 +1,6 @@
+#ifndef DECRISE_H
+#define DECRISE_H
+
+extern int dec(int a,int b);
+
+#endif
